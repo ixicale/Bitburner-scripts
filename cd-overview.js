d0d0bb796ab72dfe4ec28ac3a02ed71a078dd58a
ixicale <ixicaleescalante@gmail.com>
2022-05-04 22:22:29 -0500

    format code

--- cd-overview.js
+++ cd-overview.js
@@ -1,19 +1,15 @@
 function int_to_str(value) {
-	value = Math.floor(value)
+	value = Math.floor(value);
 	var length = (value + '').length,
 		index = Math.ceil((length - 3) / 3),
 		suffix = ['K', 'M', 'B', 'T', 'Q'];
 
 	if (length < 4) return value;
-	return (value / Math.pow(1000, index))
-		.toFixed(1)
-		.replace(/\.0$/, '') + suffix[index - 1];
-
+	return (value / Math.pow(1000, index)).toFixed(1).replace(/\.0$/, '') + suffix[index - 1];
 }
 
 /** @param {NS} ns **/
 export async function main(ns) {
-	parseFloat
 	const doc = document; // This is expensive! (25GB RAM) Perhaps there's a way around it? ;)
 	const hook0 = doc.getElementById('overview-extra-hook-0'),
 		hook1 = doc.getElementById('overview-extra-hook-1');
@@ -21,7 +17,7 @@ export async function main(ns) {
 		try {
 			let headers = [],
 				values = [];
-			const income = ns.getScriptIncome()
+			const income = ns.getScriptIncome();
 
 			headers.push('scrpt $');
 			values.push(int_to_str(income[1]) + '/s');
